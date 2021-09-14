02b239a22e5e66494f9444f379ba2a134750f7f2
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-09-14 14:59:17 -0300

    configurate first route in API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const http = require('http');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html',
-    });
+const users = require('./mocks/users');
 
-    response.end('<h1>Ola mundo</h1>');
+const server = http.createServer((request, response) => {
+    console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+
+    if(request.url === '/users' && request.method === 'GET') {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(JSON.stringify(users));
+    } else {
+        response.writeHead(400, { 'Content-Type': 'text/html' });
+        response.end(`Cannot ${request.method} ${request.url}`);
+    };
 });
 
 server.listen(3333, () => console.log('Server started at http://localhost:3333'));
