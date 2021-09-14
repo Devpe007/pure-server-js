5529a5cd49f95da48b66c170ce222e8486030dca
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-09-14 15:04:56 -0300

    solving business rule problem with UserController

---
+++ src/controllers/UserController.js
@@ -0,0 +1,8 @@
+const users = require('../mocks/users');
+
+module.exports = {
+    listUsers(request, response) {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(JSON.stringify(users));
+    },
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const http = require('http');
 
+const UserController = require('./controllers/UserController');
+
 const users = require('./mocks/users');
 
 const server = http.createServer((request, response) => {
     console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
     if(request.url === '/users' && request.method === 'GET') {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.end(JSON.stringify(users));
+        UserController.listUsers(request, response);
     } else {
         response.writeHead(400, { 'Content-Type': 'text/html' });
         response.end(`Cannot ${request.method} ${request.url}`);
